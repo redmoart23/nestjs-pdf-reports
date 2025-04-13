4c4ce058d789f1553795f5ea555601d7b9b2fae5
red <redmoart@gmail.com>
2025-04-13 11:23:07 -0500

    ✨feat: integrate pdfmake for PDF generation in basic reports 📝chore: add Roboto font files and update .gitignore 🔧fix: enable esModuleInterop in TypeScript configuration

--- src/basic-reports/basic-reports.controller.ts
+++ src/basic-reports/basic-reports.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { BasicReportsService } from './basic-reports.service';
+import { Response } from 'express';
 
 @Controller('basic-reports')
 export class BasicReportsController {
   constructor(private readonly basicReportsService: BasicReportsService) {}
 
   @Get()
-  getHello() {
-    return this.basicReportsService.getHello();
+  getHello(@Res() response: Response) {
+    const pdfDoc = this.basicReportsService.getHello();
+
+    response.setHeader('Content-Type', 'application/pdf');
+    pdfDoc.info.Title = 'Hello-World.pdf';
+
+    pdfDoc.pipe(response);
+
+    pdfDoc.end();
   }
 }
--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,6 +1,18 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma';
 
+//TODO: optimize
+import PdfPrinter from 'pdfmake';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+
+const fontDescriptors = {
+  Roboto: {
+    normal: 'fonts/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto-Medium.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf',
+  },
+};
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
@@ -8,6 +20,17 @@ export class BasicReportsService extends PrismaClient implements OnModuleInit {
   }
 
   getHello() {
-    return this.employees.findMany();
+    const printer = new PdfPrinter(fontDescriptors);
+
+    const docDefinition: TDocumentDefinitions = {
+      content: [
+        {
+          text: 'Hello world',
+        },
+      ],
+    };
+
+    const doc = printer.createPdfKitDocument(docDefinition);
+    return doc;
   }
 }
